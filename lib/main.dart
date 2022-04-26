a6fea90c63a3655f407faa9b1e5b437f62fe9c71
rajavikramsingh27 <rajavikramsingh27@gmail.com>
2022-04-26 17:15:29 +0530

    dev3+aman is merged

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,8 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
         debugShowCheckedModeBanner: false,
         home: SplashScreen()
+        // home: SplashScreen()
+        // home: SplashScreen()
     );
   }
 }
